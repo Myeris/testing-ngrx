7b4352d4f87c962d63e831e23bfab0110b52b4fe
Marie Foussette <marie.foussette@gmail.com>
2019-07-14 15:04:36 -0400

    feat(code): add prettier

--- src/app/+shared/store/sport/effects/sport.effects.spec.ts
+++ src/app/+shared/store/sport/effects/sport.effects.spec.ts
@@ -1,14 +1,14 @@
-import {async, TestBed} from '@angular/core/testing';
-import {StoreModule} from '@ngrx/store';
-import {Actions} from '@ngrx/effects';
-import {of, throwError} from 'rxjs';
-import {cold, hot} from 'jasmine-marbles';
+import { async, TestBed } from '@angular/core/testing';
+import { StoreModule } from '@ngrx/store';
+import { Actions } from '@ngrx/effects';
+import { of, throwError } from 'rxjs';
+import { cold, hot } from 'jasmine-marbles';
 // app
-import {appReducers} from '../../app.reducer';
-import {SportEffects} from './sport.effects';
-import {getActions, TestActions} from '../../../utils/test-actions.utils';
-import {SportListLoad, SportListLoadComplete, SportListLoadFail} from '../actions/sport.actions';
-import {sportList, SportResource} from '../../../resources/sport.resource';
+import { appReducers } from '../../app.reducer';
+import { SportEffects } from './sport.effects';
+import { getActions, TestActions } from '../../../utils/test-actions.utils';
+import { SportListLoad, SportListLoadComplete, SportListLoadFail } from '../actions/sport.actions';
+import { sportList, SportResource } from '../../../resources/sport.resource';
 
 describe('SportEffects', () => {
   let actions$: TestActions;
@@ -18,11 +18,7 @@ describe('SportEffects', () => {
   beforeEach(() => {
     const bed = TestBed.configureTestingModule({
       imports: [StoreModule.forRoot(appReducers)],
-      providers: [
-        SportEffects,
-        SportResource,
-        {provide: Actions, useFactory: getActions}
-      ]
+      providers: [SportEffects, SportResource, { provide: Actions, useFactory: getActions }]
     });
 
     actions$ = bed.get(Actions);
@@ -35,24 +31,23 @@ describe('SportEffects', () => {
       spyOn(sportResource, 'getSportList$').and.returnValue(of(sportList));
 
       const action = new SportListLoad();
-      const completion = new SportListLoadComplete({sports: sportList});
+      const completion = new SportListLoadComplete({ sports: sportList });
 
-      actions$.stream = hot('-a', {a: action});
-      const expected = cold('-b', {b: completion});
+      actions$.stream = hot('-a', { a: action });
+      const expected = cold('-b', { b: completion });
 
       expect(effects.loadSportList$).toBeObservable(expected);
-      sportResource.getSportList$().subscribe(result => expect(result).toEqual(sportList));
     }));
 
     it('should return a SportListLoadFail action with a string on error', async(() => {
       const action = new SportListLoad();
       const payload = 'this is an error';
-      const completion = new SportListLoadFail({error: payload});
+      const completion = new SportListLoadFail({ error: payload });
 
       spyOn(sportResource, 'getSportList$').and.callFake(() => throwError(payload));
 
-      actions$.stream = hot('-a', {a: action});
-      const expected = cold('-(c|)', {c: completion});
+      actions$.stream = hot('-a', { a: action });
+      const expected = cold('-(c|)', { c: completion });
 
       expect(effects.loadSportList$).toBeObservable(expected);
     }));
